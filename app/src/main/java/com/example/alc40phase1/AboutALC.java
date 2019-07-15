7270bfb159bcbd5d579563133a9324034303e4a0
kahuria00 <charitykahuria@gmail.com>
2019-07-15 13:25:38 +0300

    Complete Challenge

---
+++ app/src/main/java/com/example/alc40phase1/AboutALC.java
@@ -0,0 +1,68 @@
+package com.example.alc40phase1;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import org.w3c.dom.Text;
+
+public class AboutALC extends AppCompatActivity {
+    WebView webView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final ProgressBar progressBar= (ProgressBar) findViewById(R.id.progress_bar);
+        WebView webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings();
+
+
+
+
+        webView.setWebViewClient(new WebViewClient() {
+
+
+
+
+            @Override
+            public boolean shouldOverrideUrlLoading (WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+            @Override
+            public void onReceivedSslError (WebView view, SslErrorHandler handler, SslError error){
+                handler.proceed();
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int progress){
+                progressBar.setProgress(progress);
+                if(progress == 100){
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+                else {
+
+                    progressBar.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+
+
+        webView.loadUrl("https://andela.com/alc/");
+    }
+
+}
---
+++ app/src/main/java/com/example/alc40phase1/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.alc40phase1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAbout;
+    Button btnProfile;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button btn1=findViewById(R.id.btnAbout);
+        Button btn2=findViewById(R.id.btnProfile);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AboutALC.class));
+
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MyProfile.class));
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
